7d13689e0a4c05e0085f19563c61b4a45b19ec33
NathanA10 <128427225+NathanA10@users.noreply.github.com>
2023-06-13 19:25:27 -0400

    Update sql-query.sql

--- Google Data Analytics Capstone Project/SQL/sql-query.sql
+++ Google Data Analytics Capstone Project/SQL/sql-query.sql
@@ -1,1 +1,15 @@
+--Average Steps on each DayofWeek--
+SELECT 
+DayOfWeek,
+AVG(TotalSteps) as AverageSteps
 
+FROM fitbit_data.daily_activity
+
+LEFT JOIN fitbit_data.daily_sleep
+
+ON daily_activity.id = daily_sleep.id
+AND daily_activity.ActivityDate = daily_sleep.SleepDay
+
+GROUP BY DayOfWeek 
+
+ORDER BY AverageSteps DESC
