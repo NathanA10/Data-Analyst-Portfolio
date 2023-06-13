5be7672c55166c5fb0d635c81175ab8976e32f4c
NathanA10 <128427225+NathanA10@users.noreply.github.com>
2023-06-13 19:29:44 -0400

    Update sql-query.sql

--- Google Data Analytics Capstone Project/SQL/sql-query.sql
+++ Google Data Analytics Capstone Project/SQL/sql-query.sql
@@ -1,3 +1,12 @@
+--Number of participants-----
+
+SELECT 
+
+COUNT (DISTINCT ID) AS num_of_participants  
+
+FROM fitbit_data.daily_activity 
+
+
 --Average Steps on each DayofWeek--
 SELECT 
 DayOfWeek,
@@ -13,3 +22,60 @@ AND daily_activity.ActivityDate = daily_sleep.SleepDay
 GROUP BY DayOfWeek 
 
 ORDER BY AverageSteps DESC
+
+---------
+
+--Device usage on Day activity--
+SELECT 
+ID,
+COUNT(DISTINCT ActivityDate) AS num_days_used,
+CASE
+  WHEN COUNT(DISTINCT ActivityDate) >= 0 AND COUNT(DISTINCT ActivityDate) <= 10 THEN 'low usage'
+  WHEN COUNT(DISTINCT ActivityDate) >= 11 AND COUNT(DISTINCT ActivityDate) <= 20 THEN 'medium usage'
+  WHEN COUNT(DISTINCT ActivityDate) >= 21 AND COUNT(DISTINCT ActivityDate) <= 31 THEN 'high usage'
+END AS device_usage
+FROM fitbit_data.daily_activity 
+GROUP BY Id
+
+-----------
+
+--Device usage on Sleep--
+
+SELECT
+ID,
+COUNT(DISTINCT SleepDay) AS num_days_used,
+CASE
+  WHEN COUNT(DISTINCT SleepDay) >= 0 AND COUNT(DISTINCT SleepDay) <= 10 THEN 'low usage'
+  WHEN COUNT(DISTINCT SleepDay) >= 11 AND COUNT(DISTINCT SleepDay) <= 20 THEN 'medium usage'
+  WHEN COUNT(DISTINCT SleepDay) >= 21 AND COUNT(DISTINCT SleepDay) <= 31 THEN 'high usage'
+  END AS device_usage
+FROM fitbit_data.daily_sleep
+GROUP BY Id
+
+-------------------------
+
+--Intensity vs Time of Day--
+SELECT 
+MilitaryTime,
+SUM(TotalIntensity) as TotalIntensity
+
+FROM fitbit_data.hourly_intensity
+
+GROUP BY MilitaryTime
+
+ORDER BY TotalIntensity DESC
+
+
+----------------------------
+
+
+--Steps vs Time of Day--
+SELECT 
+MilitaryTime,
+SUM(StepTotal) as Totalsteps
+
+FROM fitbit_data.hourly_steps
+
+GROUP BY MilitaryTime
+
+ORDER BY Totalsteps DESC
