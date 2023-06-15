2a818aaa56225a6dfc88a8f77f0a500c15f2b3f6
NathanA10 <128427225+NathanA10@users.noreply.github.com>
2023-06-14 22:40:26 -0400

    Update and rename file1.R to graph-code.R

--- Google Data Analytics Capstone Project/R/file1.R
+++
@@ -1,2 +0,0 @@
-ggplot(data = dailyactivity, mapping = aes(x = TotalSteps, y = Calories)) + geom_point() +
-  geom_smooth() + labs(title = "Calories vs Total Steps") + xlab("Total Steps")
---
+++ Google Data Analytics Capstone Project/R/graph-code.R
@@ -0,0 +1,30 @@
+# Bar graph - Average steps of participants by hour 
+
+hourlysteps = read.csv("hourlySteps.csv")
+  
+Steps = hourlysteps %>% 
+  group_by(MilitaryTime) %>%
+  summarise(avg_step = mean(StepTotal))
+
+ggplot(data = Steps) + geom_bar(mapping=aes(x = MilitaryTime, y = avg_step), stat = "identity", fill ="dark green")+
+  theme(axis.text.x = element_text(angle = 90)) + labs(title = "Average Steps by Hour") + xlab("Time")+
+  ylab("Average Steps") + theme(plot.title = element_text(hjust = 0.5))
+
+
+
+# Bar graph - Average intensity of participants by hour
+
+hourlyintensities = read.csv("hourlyIntensities.csv")
+
+Intensity = hourlyintensities %>% 
+  group_by(MilitaryTime) %>% 
+  summarise(avg_intensity = mean(TotalIntensity))
+
+ggplot(data = Intensity) + geom_bar(mapping=aes(x = MilitaryTime, y = avg_intensity), stat = "identity", fill ="dark green")+
+  theme(axis.text.x = element_text(angle = 90)) + labs(title = "Average Intensity by Hour") + xlab("Time")+
+  ylab("Average Intensity") + theme(plot.title = element_text(hjust = 0.5))
+
+
+
+
+
